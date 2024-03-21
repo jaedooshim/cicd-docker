e0f88fa87227914c209c9307fd90558f39d201de
shimjaedoo <woen1637@naver.com>
2024-03-22 02:08:57 +0900

    chore : docker image CI/CD test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'IS CI/CD TEST';
+    return 'is CD test...auto....';
   }
 }
