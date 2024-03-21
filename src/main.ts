c4553369a16c22895ce242b6687b31bc6c079427
shimjaedoo <woen1637@naver.com>
2024-03-22 01:53:47 +0900

    chore : docker image CI/CD test

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// test11
+// test112
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
